87a00da618cb0311e361a8500e7493f0c2979ea7
Mark Williams <markzawill@gmail.com>
2016-07-02 19:52:09 -0500

    Update emotionController.js
    Deleted commented out code

--- app/Controllers/emotionController.js
+++ app/Controllers/emotionController.js
@@ -1,4 +1,4 @@
-// "use strict";
+
 
 "use strict";
 
@@ -19,46 +19,3 @@ Pickture.controller('emotionController', [
 
 ]);
 
-
-// Pickture.controller('authController', [
-//   '$http', 
-//   '$scope',
-//   'authFactory',
-  
-//     function($http, $scope) {
-//     	$scope.emotions = [];
-
-//         var params = {
-//         	$http
-// 			.get("http://localhost:57677/api/image")
-// 			.success(img => $scope.images = img);
-// 			console.log($scope.images);
-            
-//         };
-      
-//         $http({
-//             url: "https://api.projectoxford.ai/emotion/v1.0/recognize?" + params;
-//             beforeSend: function(xhrObj){
-//                 // Request headers
-//                 xhrObj.setRequestHeader("Content-Type","application/json");
-//                 xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","{subscription key}");
-//             },
-
-
-//             type: "POST",
-//             data :JSON.stringify(data),
-//             url: "api/image",
-//             contentType: "application/json"
-//             // data: "{body}",
-
-//         })
-//         .success(function(data) {
-           
-//             .success(emo => $scope.emotions = );
-// 			console.log($scope.images);
-//         })
-//         .fail(function() {
-//             alert("error");
-//         });
-//         console.log(data);
-//     };
